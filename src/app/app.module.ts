560c07e7cf67f3ec0104b2521e95be06b24ead3f
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2022-12-27 00:36:03 +0100

    Pass first tutorial chapter: 'The hero editor'
    implements HeroesComponent with one hero 'Windstorm', implements 'Hero' interface, display HeroesComponent on main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
+// Angular core imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
+// Modules imports
 import { AppRoutingModule } from './app-routing.module';
+
+// Components imports
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
